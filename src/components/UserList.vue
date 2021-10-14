8251b7c55deb4213a66b395cf23254356c8fd99c
Shayan <ssaif@kinaxis.com>
2021-10-14 13:00:38 -0400

    Initial commit

---
+++ src/components/UserList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="user-list">
+    <div v-if="users">
+      <pre>{{ users }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "UserList",
+  data() {
+    return {
+      users: {
+        type: Object,
+      },
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    ...mapActions(["loadUsers"]),
+    async fetchUsers() {
+      this.loadUsers();
+      this.users = await this.$store.state.users;
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
+    users: [],
   },
   mutations: {
+    setUsers(state, users) {
+      state.users = users;
+    },
   },
   actions: {
+    async loadUsers({ commit }) {
+      const res = await axios.get('https://randomuser.me/api/?results=5');
+      const data = await res.data;
+      
+      commit('setUsers', data.results);
+    }
   },
   modules: {
   }
